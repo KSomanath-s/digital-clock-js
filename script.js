50294d092d5252e9331f85694e7c8bec55f1f6cf
gundapdhanaji <gundapdhanaji@gmail.com>
2025-11-03 10:46:39 +0530

    removed selector text outside of the time function

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
+const digitalTime = document.querySelector("#digitalTime");
+
 setInterval(() => {
-    const digitalTime = document.querySelector("#digitalTime");
     const newTime = new Date();
     digitalTime.textContent = newTime.toLocaleTimeString();
 }, 1000);
